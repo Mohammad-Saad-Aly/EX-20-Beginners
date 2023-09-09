54c18437a68c9c7f5f0b3ca06398c841b8234ba6
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-09-09 15:43:11 +0500

    new classes

---
+++ javascript/class 1/app.js
@@ -0,0 +1,51 @@
+// alert('hello world')
+// document.write('<h1>'+ 'hello world' + '</h1>')
+
+// var num1 = 90
+// var num2 = "10"
+// var num3 = 5
+
+// document.write(num1 + num2 + num3)
+
+
+// var num1 = +prompt('Enter value 1')
+// var num2 = +prompt('Enter value 2')
+// var add = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var mod = num1 % num2
+
+// document.write('The addition of '+num1 + ' and '+ num2+" is "+add +"<br>")
+
+
+// document.write('The Subtraction of '+num1 + ' and '+ num2+" is "+sub +"<br>")
+
+
+// document.write('The Multiply of '+num1 + ' and '+ num2+" is "+mul +"<br>")
+
+
+// document.write('The division of '+num1 + ' and '+ num2+" is "+div +"<br>")
+
+
+// document.write('The modulus of '+num1 + ' and '+ num2+" is "+mod +"<br>")
+
+
+// var num1 = 2
+// var num2 = 3
+// var num3 = 4
+
+// var eq = num1 + num2 * num3 - num1
+//         //   2   +   3  *  4   - 2
+
+//         document.write(eq)
+
+document.write(4 + " x °C "+1 + ' = '+ 4*1+'<br>')
+document.write(4 + " x "+2 + ' = '+ 4*2+'<br>')
+
+
+
+
+
+
+
