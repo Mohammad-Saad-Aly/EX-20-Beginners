d1429f42acef2db5c970d5c061f6c00e126c58bf
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-09-10 15:54:23 +0500

    chapter 9 to 13

---
+++ javascript/class 2/app.js
@@ -0,0 +1,153 @@
+// document.write('6' + " x " + "1" + " = " + 6 * 1+"<br>")
+// document.write('6' + " x " + "2" + " = " + 6 * 2+"<br>")
+// document.write('6' + " x " + "3" + " = " + 6 * 3+"<br>")
+// document.write('6' + " x " + "4" + " = " + 6 * 4+"<br>")
+// document.write('6' + " x " + "5" + " = " + 6 * 5+"<br>")
+// document.write('6' + " x " + "6" + " = " + 6 * 6+"<br>")
+// document.write('6' + " x " + "7" + " = " + 6 * 7+"<br>")
+// document.write('6' + " x " + "8" + " = " + 6 * 8+"<br>")
+// document.write('6' + " x " + "9" + " = " + 6 * 9+"<br>")
+// document.write('6' + " x " + "10" + " = " + 6 * 10)
+
+
+
+// var userInp = +prompt('Calcius lelo')
+// var fahrenheit = +prompt('Fahrenheit lelo')
+// var userInp = 12
+
+// var form = userInp * 1.8 + 32
+// var form2 = (fahrenheit - 32) * 5 / 9
+// document.write(form)
+
+
+
+// var num1 = 5
+// var num2 = 10
+// var num3 = 2
+// var num4 = 3
+// var num5 = 4
+
+// var i = 5 + 5 - 7 + 10 + num4++ - num3-- + ++num4 + num2-- + num5++ + 1
+//             // 13         +  3     -  2     +   5    +  10    +  4    + 1
+
+//             document.write(i)
+
+
+
+
+// var firstName = true
+
+// if(firstName == 'true'){
+//     document.write('han naam theak hai')
+// }
+// else{
+//     document.write('galat hai')
+// }
+
+
+// var eng = +prompt('Enter English Marks')
+// var urdu = +prompt('Enter urdu Marks')
+// var pst = +prompt('Enter pst  Marks')
+// var isl = +prompt('Enter isl Marks')
+// var maths = +prompt('Enter maths Marks')
+// var total = eng + urdu + pst + isl + maths
+// var perc = (total / 500) * 100
+
+// document.write(
+
+//     '<table border="1px">'+
+//     '<tr>'+
+//     '<th>'+'English'+'</th>'+
+//     '<th>'+'Urdu'+'</th>'+
+//     '<th>'+'PST'+'</th>'+
+//     '<th>'+'ISLAMIAT'+'</th>'+
+//     '<th>'+'MATHS'+'</th>'+
+//     '<th>'+'TOTAL'+'</th>'+
+//     '<th>'+'PERCENTAGE'+'</th>'
+//     +'</tr>'+
+//     '<tr>'+
+//     '<td>'+eng+'</td>'+
+//     '<td>'+urdu+'</td>'+
+//     '<td>'+pst+'</td>'+
+//     '<td>'+isl+'</td>'+
+//     '<td>'+maths+'</td>'+
+//     '<td>'+total+'</td>'+
+//     '<td>'+perc.toFixed(2)+"%"+'</td>'
+    
+//     +'</tr>'    
+//     +'</table>'
+// )
+
+var userCity = prompt('Enter city Name')
+
+if(userCity == 'karachi'){
+    document.write('City of Lights')
+}
+else if(userCity == 'lahore'){
+    document.write('khottey khaao')
+}
+else if(userCity == 'multan'){
+    document.write('Halwa le aao')
+}
+else{
+    document.write('chal nikal')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
