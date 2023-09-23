7346c035c1f7dcdb131bfcd1162bc0c36f7b8b36
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-09-23 13:50:40 +0500

    for loop

---
+++ javascript/class 4/app.js
@@ -0,0 +1,159 @@
+// var userInp = prompt('Enter Email','saad@gmail.com')
+
+// if(userInp == 'abc@gmail.com' || userInp == 'def@gmail.com' || userInp == 'xyz@gmail.com'){
+//     alert('login successful')
+// }
+// else{
+//     alert('Invalid')
+// }
+
+// var userInp = +prompt('Enter name')
+
+// if(userInp == 0 ){
+//     alert('khali hai')
+// }
+// else{
+//     alert('bhara hua hai    ')
+// }
+
+// loop
+
+// for loop
+// while loop
+// do while loop
+
+
+// initialization
+// condition check
+// inc / dec
+
+
+// var abc; // dec
+
+// abc = 0 // ini
+
+
+
+
+// var i;
+
+// for (i = 11; i <= 10; i++) {
+//     document.write(i,"<br>")
+// }
+
+// var rr = 'hello world'
+
+// for(var i = 1; i <= 20; i++){
+//     document.write(i+rr+"<br>")
+// }
+
+// for (var i = 55; i >= 10; i--) {
+//     document.write(i, "<br>")
+// }
+// var abc = prompt('Enter table')
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(abc + " x "+ i + " = " + abc*i+"<br>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
