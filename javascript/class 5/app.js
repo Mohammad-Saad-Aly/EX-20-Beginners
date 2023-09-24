adcc9b8872ebe099c816cebe69179bad71748df4
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-09-24 15:45:53 +0500

    nested loops

---
+++ javascript/class 5/app.js
@@ -0,0 +1,188 @@
+// loop
+// for false ==> //
+// while false ==> //
+// do while ==> ./
+
+// var i;
+// for (i = 0; i <= 10; i++){
+//     document.write(i+') hello world <br>')
+// }
+
+
+// for(var i=100; i >=50; i--){
+//     document.write(i,"<br>")
+// }
+
+// var arr = ['Jalal', 'Osama']
+// arr.push('Kathori') // akhir jal osama kathori
+// arr.unshift('Shabbir') // shru shab
+
+// arr.shift() // shru delete jalal osama kathori
+// arr.splice(2,3)
+
+// document.write(arr)
+
+// var arr = []
+
+// arr.push('Ali')
+// arr.unshift('Hamza')
+// arr.shift()
+// arr.pop()
+// arr.unshift('Rehman','Wasif')
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.push('Rehman 2','Taha','Shabbir')
+
+// document.write(arr)
+
+// var arr = ['iPhone', 'Samsung','Oppo', 'Motorola', 'infinix']
+
+// document.write('<select>')
+
+// for(var i=0; i<arr.length; i++){
+//     document.write(
+//         '<option>'+arr[i]+'</option>'
+//     )
+// }
+
+// document.write('</select>')
+
+// for(var i=0; i < arr.length; i++){
+//     document.write(arr[i],"<br>")
+// }
+
+// document.write(
+
+//     '<select>'+
+//     '<option>'+arr[0]+'</option>'+
+//     '<option>'+arr[1]+'</option>'+
+//     '<option>'+arr[2]+'</option>'+
+//     '<option>'+arr[3]+'</option>'+
+//     '</select>'
+// )
+
+
+// falsy values
+
+// 1) 0
+// 2) false
+// 3) NaN
+// 4) undefined
+// 5) ''
+// 6) null
+
+// var a = 4
+// var b = 5
+// var c = 10
+
+// var res = a++ + ++a + a + b++ + c-- + c-- - --c + a++ + b++ + ++b - --b - --b - --b + c++
+//         //    4  +  6  + 6 +  5  +  10 +  9  -  7  +  6  +  6  +  8  -  7  -  6  -  5  + 7
+
+//  document.write(res)
+
+
+// nested loop
+
+// var arr = ['Saad', 'Ali', 'Hamza', 'Rehman', 'Taha']
+// var arr2 = ['Khan', 'Gujjar']
+
+
+// for(var i=0; i < arr.length; i++){
+
+//     for(var j=0; j < arr2.length; j++){
+
+//         document.write(arr[i] + " "+ arr2[j] + "<br>")
+
+//     }
+
+// }
+
+// saad khan
+// saad gujjar
+
+// ali khan
+// ali gujjar
+
+
+// for(var i=1; i<=5; i++){
+//     for(var j=1; j <= i; j++){
+//         document.write(j+" ")
+//     }
+//     document.write('<br>')
+// }
+
+
+
+
+
+
+
+
+
+
+
+// 1 
+// 1 2 
+// 1 2 3
+// 1 2 3 4 
+// 1 2 3 4 5
+
+// 1 2 3 4 5 
+// 1 2 3 4 5
+// 1 2 3 4 5 
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+// 5 
+// 5 4 
+// 5 4 3 
+// 5 4 3 2 
+// 5 4 3 2 1
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+
+
+
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= i; j++)
+//     {
+//         document.write(j+" ")
+//     }
+//     document.write('<br>')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
