846ee7a214d9aff8f72a3c6dcd92f7f6d73295b6
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-09-30 16:07:49 +0500

    new class

---
+++ javascript/class 6/app.js
@@ -0,0 +1,191 @@
+// for(var i=5; i>=1; i--){
+//     for(var j=5; j>=i; j--){
+//         document.write(j," ")
+//     }
+//     document.write('<br>')
+// }
+
+// var r = 5
+// var e = 2
+// var res = e++ - e-- - ++e + r++ + ++r + 5 + 10 + e++ - --e
+//         //    2  -  3  -  3  +  5  +  7  + 5 + 10 +  3  -  3
+
+// document.write(res)
+
+
+// var userInp = prompt('Enter Dish name')
+// var arr = ['Biryani','Qoftey','Haleem','BBQ','Mandi','Ande wala burger']
+
+// var bool = false
+
+// for(var i=0; i< arr.length; i++){
+//     if(userInp === arr[i]){
+//         // bool = true
+//         alert('Dish Found')
+//         break
+//     }
+// }
+
+// if(bool == false){
+//     alert('Not Found')
+// }
+
+
+// multan // halwa
+// lahore // gosht
+// karachi // biryani
+// peshawar // naswar
+// quetta // chae
+
+// 'you selected '+ cityname + ' ye hai iski khasiyat'
+
+// var userInp = prompt('Enter city Name')
+// var cityNames = ['multan', 'lahore']
+// var check = 'koi dusri value'
+
+
+// for (var i = 0; i < cityNames.length; i++){
+//     if(userInp === cityNames[i]){
+//         check = 'koi nayi value'
+//         var selectedCity = cityNames[i]
+//         if(selectedCity == 'multan'){
+//             alert('halwa')
+//             break
+//         }
+//         else if(selectedCity  == 'lahore'){
+//             alert('Gosht')
+//             break
+//         }
+//     }
+// }
+
+// if(check == 'koi dusri value'){
+//     alert('not Found')
+// }
+
+// var userInp = prompt('Enter Name')
+// var first = userInp.slice(0,1).toUpperCase()
+
+// var second = userInp.slice(1).toLowerCase()
+// var final = first+second
+
+// document.write(final)
+
+
+// var userInp = prompt('Enter City name')
+// var arr = ['Karachi','Multan','Lahore','Islamabad']
+
+// var first = userInp.slice(0,1).toUpperCase()
+// var second = userInp.slice(1).toLowerCase()
+// var final = first + second
+// var match = false
+// for(var i=0; i<arr.length; i++){
+//     if(final == arr[i]){
+//         match = true
+//         alert('City Found')
+//         break
+//     }
+// }
+// if(match === false){
+//     alert('Not Found')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var name1 = 'Asad'
+// document.write(name1[1].toUpperCase())
+
+// ['a','s','a','d']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
